3ffcb57248a31d0dedd81832783c2b148c411ce7
Hang Hu <huhang600@gmail.com>
2019-05-16 21:22:44 -0700

    2019-05-16 new posts

--- push_post.sh
+++ push_post.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
 echo "$DATE new posts"
-<<<<<<< HEAD
-cd /Users/hanghu/Documents/CodeSpace/Other/Hang-Hu.github.io/
-=======
-cd ~/Documents/gitRepository/My-Git/Hang-Hu.github.com/
->>>>>>> 1c11919a5d9c64bb30a704f87c9f1c1814145f10
+cd ${POST_HOME}
 git pull
 git add .
 git commit -m "$DATE new posts"
